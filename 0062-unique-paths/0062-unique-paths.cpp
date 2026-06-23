ab8c955ce84bc015b93b28b96f2e2d6637039927
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-06-23 15:24:09 +0530

    Time: 0 ms (100%), Space: 9.4 MB (52.69%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+
+        vector<vector<int>>numberofuniquepaths(m , vector<int>(n));
+
+        for(int i=0;i<m;i++){
+            numberofuniquepaths[i][0] =1;
+
+        }
+        for(int i=0;i<n;i++){
+            numberofuniquepaths[0][i]=1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                numberofuniquepaths[i][j] = numberofuniquepaths[i][j-1] + numberofuniquepaths[i-1][j];
+            }
+        }
+        return numberofuniquepaths[m-1][n-1];
+
+        
+    }
+};
