15bc459735a6c5733c405413e35fb775773a1538
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-06-24 11:10:13 +0530

    Time: 48 ms (57.33%), Space: 15.7 MB (63.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -20,7 +20,7 @@ public:
 
         for(int i=1;i<=s1;i++){
             for(int j=1;j<=s2;j++){
-                if(s[i-1] == p[j-1] || p[i-1] == '?') dp[i][j] = dp[i-1][j-1];
+                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
                 else if (p[j-1]=='*') dp[i][j] = dp[i-1][j] || dp[i][j-1]; //* cna be empty too 
                 else dp[i][j]=false;
             }
