6bdf2cfd1f88e1bfd6e8af0558a03418611f8d4c
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-06-24 11:07:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+
+        int s1=s.size();
+        int s2=p.size();
+        vector<vector<bool>> dp(s1+1 , vector<bool>(s2+1));
+
+        dp[0][0]= true;
+        for(int i=1;i<=s1;i++){
+            dp[i][0] = false;
+        }
+
+        for(int i=1 ; i<=s2;i++){
+            if(p[i-1] =='*') dp[0][i] = dp[0][i-1];
+            else dp[0][i]=false;
+        }
+
+        // base done
+
+        for(int i=1;i<=s1;i++){
+            for(int j=1;j<=s2;j++){
+                if(s[i-1] == p[j-1] || p[i-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if (p[j-1]=='*') dp[i][j] = dp[i-1][j];
+                else dp[i][j]=false;
+            }
+        }
+        return dp[s1][s2];
+    }
+};
