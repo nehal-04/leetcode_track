00ee31c3759926f73d706d320a695565ba729ea2
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-07-01 16:00:31 +0530

    Time: 543 ms (87.13%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 Select round(count(distinct A1.player_id) / (select count(distinct player_id) from Activity) ,2) as fraction
-from Activity A1 inner join (Select player_id , min(event_date) as event_date from Activity group by player_id) A2 on A1.player_id = A2.player_id and A1.event_date = A2.event_date +1
+from Activity A1 inner join (Select player_id , min(event_date) as event_date from Activity group by player_id) A2 on A1.player_id = A2.player_id and A1.event_date=DATE_ADD(A2.event_date, Interval 1 day)
