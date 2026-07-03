5611e652ecaced894c26ab58751d3238721d2392
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-07-03 10:17:58 +0530

    Time: 162 ms (5.03%), Space: 75.7 MB (32.82%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergesort2sortedlists(ListNode* A, ListNode* B){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr=dummy;
+        while(A!=nullptr && B!=nullptr){
+            if(A->val <= B->val){
+                curr->next=A;
+                A=A->next;
+                curr=curr->next;
+            }
+            else{
+                curr->next=B;
+                B=B->next;
+                curr=curr->next;
+            }
+        }
+        while(A!=nullptr){
+            curr->next=A;
+            A=A->next;
+            curr=curr->next;
+        }
+
+        while(B!=nullptr){
+            curr->next=B;
+            B=B->next;
+            curr=curr->next;
+        }
+
+        curr->next=nullptr;
+        return dummy->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr || head->next ==nullptr) return head;
+        ListNode* temp=head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev=nullptr;
+        while(fast!=nullptr && fast->next != nullptr){
+            fast=fast->next->next;
+            prev=slow;
+            slow=slow->next;
+        }
+        ListNode* mid=nullptr;
+        if(fast == nullptr){
+            //even length list
+            prev->next=nullptr;
+            mid=slow;
+        }
+        else{
+            // in an odd length list , left side more elemtns right side less
+            mid=slow->next;
+            slow->next=nullptr;
+        }
+        
+
+        ListNode* A = sortList(temp);
+        ListNode* B = sortList(mid);
+        return mergesort2sortedlists(A,B);
+
+
+
+        
+    }
+};
