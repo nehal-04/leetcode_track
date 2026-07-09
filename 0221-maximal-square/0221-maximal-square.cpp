6995474f95bdfb737e0dd7c783c8d3c017c80009
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-07-09 19:06:53 +0530

    Time: 7 ms (74.13%), Space: 27.4 MB (98.12%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,28 +3,20 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int rows=matrix.size();
         int cols=matrix[0].size();
-        vector<vector<int>>dp(rows+1,vector<int>(cols+1));
-
-        // BASE CASES   
-        for(int i=0;i<=cols;i++) dp[0][i]=0;
-        for(int i=1;i<=rows;i++) dp[i][0]=0;
+        vector<int>prev(cols+1,0);
+        vector<int>curr(cols+1,0);
 
         int maxans=0;
         for(int i=1;i<=rows;i++){
             for(int j=1;j<=cols;j++){
-                if(matrix[i-1][j-1] == '0') {
-                    dp[i][j] =0;
+                if(matrix[i-1][j-1] == '0'){
+                    curr[j]=0;
                     continue;
                 }
-                int top_sidelen = dp[i-1][j];
-                int left_sidelen = dp[i][j-1];
-                int diagonal_sidelen = dp[i-1][j-1];
-
-                int minlen= min({top_sidelen , left_sidelen , diagonal_sidelen});
-                dp[i][j] = minlen+1;
-                maxans= max(maxans , dp[i][j]);
-
+                curr[j] = min({curr[j-1] , prev[j] , prev[j-1]}) + 1;
+                maxans = max(maxans , curr[j]); 
             }
+            prev=curr;
         }
         return maxans * maxans;
 
