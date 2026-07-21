82a97f6acf34dcacb66b8aa5e501fc5b75f40fb9
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-07-21 23:17:56 +0530

    Time: 0 ms (100%), Space: 9.4 MB (57.78%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -1,39 +1,18 @@
 class Solution {
-private:
-    bool helper(string &s){
-        int n=s.size();
-        string t = s;
-
-        for(int i=0;i<n-1;i++){
-            if(s[i]=='0' && s[i+1]=='1'){
-                t[i]='1';
-                t[i+1]='0';
-            }
-        }
-        s = t;
-        for(int i=0;i<n-1;i++){
-            if(s[i]=='0' && s[i+1]=='1') return false;
-        }
-        return true;
-    }
 public:
     int secondsToRemoveOccurrences(string s) {
-        int steps=0;
-        bool check=false;
-        for(int i=0;i<s.size()-1;i++){
-            if(s[i]=='0' && s[i+1]=='1') {
-                check=true;
-                break;
+        int prev_fin =-1;
+        int fin_fin=0;
+        int zeroes=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i] == '0') zeroes++;
+            else if(zeroes>0) {
+                fin_fin = max(zeroes , prev_fin + 1);
+                prev_fin = fin_fin;
             }
         }
-        if(!check) return 0;
-        while(true){
-            bool ans=helper(s);
-            steps++;
-            if(ans) return steps;
-        }
-        return steps;
-
+        return fin_fin;
 
         
     }
