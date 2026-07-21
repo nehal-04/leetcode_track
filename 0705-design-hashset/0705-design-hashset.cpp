27925c3489ce5454001badf85106f6e95af03493
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-07-22 00:41:43 +0530

    Time: 23 ms (31.63%), Space: 49.3 MB (67.63%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,43 +1,20 @@
 class MyHashSet {
-private:
-    vector<vector<int>>v;
 public:
-    MyHashSet() {
-        v.resize(1009);    
-    }
-    
-    void add(int key) {
-       int idx = key%1009 ;
-       for(size_t i=0;i<v[idx].size();i++) {
-        if(v[idx][i] == key) return;
-       }
-       v[idx].push_back(key); 
-    }
-    
-    void remove(int key) {
-        int idx = key%1009;
-        for(size_t i =0 ; i < v[idx].size();i++){
-            if(v[idx][i] == key){
-                v[idx].erase(v[idx].begin() + i);
-                return;
-            }
-        }
-    }
-    
-    bool contains(int key) {
-        int idx = key%1009;
-        for(size_t i=0;i<v[idx].size();i++){
-            if(v[idx][i] == key) return true;
-        }
-        return false;
+    vector<bool> hash;
 
+    MyHashSet() {
+        hash.resize(1000001, false);
+    }
+
+    void add(int key) {
+        hash[key] = true;
+    }
+
+    void remove(int key) {
+        hash[key] = false;
+    }
+
+    bool contains(int key) {
+        return hash[key];
     }
 };
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
