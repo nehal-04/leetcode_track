eb2604dad75da33ccf89c2a776936d4e05f76e1f
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-07-29 20:59:03 +0530

    Time: 0 ms (100%), Space: 80.1 MB (8.42%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
@@ -15,44 +15,17 @@ public:
 
 
         vector<long long> helper(n);
+        long long  ops=0;
         for(int i=0;i<n;i++){
             long long diff = nums1[i]-nums2[i];
             if(abs(diff) % k != 0) return -1;
             total_diff += diff;
             helper[i] = diff;
+            if(diff<0) ops += abs(diff)/k;
         }
 
         if(total_diff != 0) return -1;
-
-        sort(helper.begin() , helper.end());
-        int i=0;
-        int j = n-1;
-        long long ops = 0;
-        while(i<j){
-            if(helper[i] == 0){
-                i++;
-                continue;
-            }
-            else if (helper[j] == 0){
-                j--;
-                continue;
-            }
-            else if(helper[i] >0 && helper[j] > 0 ) return -1;
-            else if(helper[i] < 0 && helper[j] < 0) return -1;
-
-            int take = min(-1*helper[i] , helper[j]);
-
-            helper[i] += take;
-            helper[j] -=take;
-            ops += take/k;
-            if(helper[i] == 0) i++;
-            if(helper[j] == 0) j--; 
-        }
-
         return ops;
-
-
-
 
         
     }
