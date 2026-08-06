4184612532619e64910305127b206836bde4c3fb
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-08-06 22:22:35 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int productofdigits(int n){
+        int ans =1;
+        while(n>0){
+            int d=n%10;
+            ans*=d;
+            n=n/10;
+        }
+        return ans;
+    }
+public:
+    int smallestNumber(int n, int t) {
+        int i=n;
+
+        while(true){
+            int prod = productofdigits(i);
+            if(prod % t == 0) return i;
+            i++;
+        }
+        return -1;
+        
+    }
+};
