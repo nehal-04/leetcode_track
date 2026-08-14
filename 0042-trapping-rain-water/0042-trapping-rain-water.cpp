10c1f06583523b34c104c7c52b8caaa7ea283be6
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-08-14 11:06:05 +0530

    Time: 0 ms (100%), Space: 26.6 MB (41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,37 +1,36 @@
 class Solution {
 private:
-    void makenge(vector<int>&height , vector<int>&arr){
-        arr[height.size()-1]=height[height.size()-1];
-        int max_to_right = height[height.size()-1];
-        bool check=false;
-        int val;
+    void makemaxtoright(vector<int>&height , vector<int>&arr){
+        arr[height.size()-1] = height[height.size()-1];
+        int max_to_right= height[height.size()-1];
         for(int i=height.size()-2;i>=0;i--){
-            if(height[i]>max_to_right) {check=true;val=height[i];}
-            arr[i]=max_to_right;
-            if(check){
-                max_to_right=val;
-                check=false;
-            };
+            arr[i]= max_to_right;
+            max_to_right = max(height[i] , max_to_right);
         }
-
-
-        
     }
 public:
     int trap(vector<int>& height) {
+        int n = height.size();
+        int total=0;
+        int max_to_left = height[0];
+        vector<int>arr(n,0);
+        makemaxtoright(height,arr);
+
+        for(int i=1;i<n;i++){
+
+            if(height[i] >= max_to_left || height[i] >= arr[i]) {
+                max_to_left = height[i];
+                continue;
+            }
+
+            int val_added = min(arr[i], max_to_left) - height[i];
+            total += val_added;
+            max_to_left = max(max_to_left , height[i]);
         
-        int prefix_max=height[0];
-        int tot_water=0;
-        vector<int>next_max_right(height.size());
-        makenge(height,next_max_right);
-
-        for(int i=1;i<height.size()-1;i++){
-            int water_added = min(next_max_right[i] , prefix_max) -height[i];
-            if(water_added > 0 ) tot_water += water_added;
-            prefix_max=max(prefix_max , height[i]);
         }
-        return tot_water;
-
+        return total;
+        
         
     }
+    
 };
