9afd84bf55eb94a41191871a12403286884304f1
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-08-14 12:51:00 +0530

    Time: 15 ms (6.85%), Space: 15.1 MB (5.13%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-
-        int n=num.size();
-        if(num=="0") return "0";
-        string ans="";
-
+        if(num=="0" || num =="1") return "0";
+        
+        int n = num.size();
+        string ans ="";
         stack<int>st;
-        st.push(num[0]-'0');
+        st.push(num[0] -'0');
         for(int i=1;i<n;i++){
-            while(k>0 && !st.empty() && st.top()  > num[i]-'0' ) {st.pop();k--;}
-            st.push(num[i]-'0');
+            while(k>0 && !st.empty() && st.top()> num[i] - '0'){
+                st.pop();
+                k--;
+            }
+            st.push(num[i] -'0');
         }
 
-        //for cases like 123456
+        // for cases like 123456
         while(k>0){
+            st.pop();
             k--;
-            st.pop();
         }
 
-        if(st.empty())return "0";
+        // now number at top should come at end first put it at the beginnign and check
         while(!st.empty()){
-            ans+=to_string(st.top());
+            ans += to_string(st.top());
             st.pop();
         }
 
-        //imp if answer seems to be starting with 0 , remove the 0s
-        while(ans.size()>0 && ans.back()=='0'){
+        // 00000 if in the end 
+        while(ans.size() > 0 && ans.back()=='0'){
             ans.pop_back();
         }
-        if(ans.empty())return "0";
+        if(ans.size() == 0) return "0";
         reverse(ans.begin() , ans.end());
-        
         return ans;
-
-
-
-
-
-
-
-
-
-        
     }
 };
