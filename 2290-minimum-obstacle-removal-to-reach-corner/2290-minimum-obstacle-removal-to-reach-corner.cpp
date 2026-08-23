d806d7a85b68e0a92a68501115d1f6b623e8a7bc
Nehal <140091260+nehal-04@users.noreply.github.com>
2026-08-23 16:37:55 +0530

    Time: 111 ms (79.46%), Space: 121.3 MB (58.84%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -3,37 +3,35 @@ public:
     int minimumObstacles(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int , pair<int,int>>>>pq;
+
         vector<vector<int>> dist(r,vector<int>(c,INT_MAX));
-        
-        pq.push({0,{0,0}});
-        dist[0][0]=0;
+        deque<pair<int , int>> dq;
 
-        vector<int>dr = {0,0,-1,1};
-        vector<int>dc = {1 , -1 ,0 ,0};
+        dq.push_back({0,0});
+        dist[0][0] = 0;
 
-        while(!pq.empty()){
-            int cur_dist = pq.top().first;
-            int cur_r = pq.top().second.first;
-            int cur_c = pq.top().second.second;
-            
-            pq.pop();
+        vector<int>dr = {0,0,1,-1};
+        vector<int>dc = {1 , -1 , 0 , 0};
 
-            if(cur_dist > dist[cur_r][cur_c]) continue;
+        while(!dq.empty()){
+            auto[cur_r , cur_c]= dq.front();
+            dq.pop_front();
 
             for(int i=0;i<4;i++){
                 int new_r = cur_r + dr[i];
                 int new_c = cur_c + dc[i];
-                if(new_r <0 || new_c <0 || new_r == r || new_c == c) continue;
-                int edge_wt = (grid[new_r][new_c] == 1) ? 1 : 0;
+                if(new_r <0 || new_c < 0 || new_r >= r || new_c >=c)continue;
 
-                if(cur_dist + edge_wt < dist[new_r][new_c]){
-                    dist[new_r][new_c] = cur_dist + edge_wt;
-                    pq.push({dist[new_r][new_c] , {new_r , new_c}});
+                int edge_wt = grid[new_r][new_c];
+                if(dist[cur_r][cur_c] + edge_wt < dist[new_r][new_c]){
+                    dist[new_r][new_c] = dist[cur_r][cur_c] + edge_wt;
+                    if(edge_wt ==0) dq.push_front({new_r , new_c});
+                    else dq.push_back({new_r , new_c});
                 }
-            }
 
+            }
         }
         return dist[r-1][c-1];
+
     }
 };
